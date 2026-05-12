bdaa7eb54533e8a9f75a81c1fd7300f032a6b02e
Yan Maia <yandantasmaia12@gmail.com>
2026-05-12 17:49:19 +0100

    Adicionando tudo fora do src

---
+++ centavos/src/test/java/com/yanmaia12/centavos/CentavosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yanmaia12.centavos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentavosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
