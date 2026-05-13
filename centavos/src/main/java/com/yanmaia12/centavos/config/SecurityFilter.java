9d083f910bc524a4b3b21cdd7283cb095ff9b191
Yan Maia <yandantasmaia12@gmail.com>
2026-05-13 10:29:08 +0100

    Adicionando SecurityFilter

--- centavos/src/main/java/com/yanmaia12/centavos/config/SecurityFilter.java
+++ centavos/src/main/java/com/yanmaia12/centavos/config/SecurityFilter.java
@@ -1,4 +1,53 @@
 package com.yanmaia12.centavos.config;
 
-public class SecurityFilter {
+import com.yanmaia12.centavos.model.Usuario;
+import com.yanmaia12.centavos.repository.UsuarioRepository;
+import com.yanmaia12.centavos.service.UsuarioService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+    private final JwtService jwtService;
+    private final UsuarioRepository usuarioRepository;
+
+    public SecurityFilter(JwtService jwtService, UsuarioRepository usuarioRepository) {
+        this.jwtService = jwtService;
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recuperarToken(request);
+
+        if (token != null){
+            String email = jwtService.validarToken(token);
+
+            if (email.isEmpty()){
+                Usuario usuario = usuarioRepository.findByEmail(email).orElse(null);
+
+                if (usuario != null){
+                    var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            }
+        }
+        
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if (authHeader == null) return null;
+        return authHeader.replace("Bearer ", "");
+    }
 }
